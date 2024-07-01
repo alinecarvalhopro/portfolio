1f854dae9319166413faa8e2e5e284df46254896
aline <alinecarvalho.pro@gmail.com>
2024-07-01 15:30:38 -0300

    feat: Update skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -34,21 +34,42 @@ export const Skills = () => {
         </h3>{' '}
         <br />
         <p>
-          Atualmente, trabalho com o desenvolvimento de aplicativos mobile,
-          utilizando a tecnologia React Native, oferecendo a solução completa
-          para o cliente, desde o aplicativo em si até o desenvolvimento do
-          painel administrativo, em React.js.
+          Tenho experiência com o desenvolvimento de aplicativos mobile,
+          utilizando a tecnologia React Native, tanto com CLI quanto com EXPO,
+          desenvolvendo a aplicação da forma mais fiel possível ao protótipo
+          “pixel perfect”.
         </p>{' '}
         <br />
         <p>
-          Como desenvolvedora full-stack, também sou responsável pela construção
-          da api, em Node.js, Sequelize e Banco de dados MySQL.
+          Amplo conhecimento em CSS e estilização com as bibliotecas, como
+          Tailwind, Styled Components e Sass.
         </p>{' '}
         <br />
         <p>
-          Porém minha especialidade é em front-end, desenvolvendo a aplicação da
-          forma mais fiel possível ao protótipo “pixel perfect” e trazendo
-          soluções também que melhorem a experiência do usuário.
+          Integração e consumo com APIs. Utilização de React Hooks e de
+          bibliotecas para as mais diversas funcionalidades. Desenvolvimento
+          baseado em componentes.
+        </p>{' '}
+        <br />
+        <p>
+          Além disso, já tive a experiência gerando AAB e APK de aplicativos e
+          em deploy / publicação na Google Play. 
+        </p>{' '}
+        <br />
+        <p>
+          Também tenho experiência no desenvolvimento web utilizando React.js e
+          Next.js*.
+        </p>{' '}
+        <br />
+        <p>
+          <small>
+            *Inclusive, um detalhe interessante sobre isso é que esse projeto,
+            meu portfólio pessoal, foi desenvolvido em Next.js, TypeScript e
+            estilizado com a biblioteca Sass. Ele conta com 3 layouts diferentes
+            para responsividade, além da versão mobile e tamanhos mais comuns de
+            tela, ele conta com layout exclusivo para tamanhos gigantes. Não
+            deixe de testar.
+          </small>
         </p>{' '}
         <br />
         <h3 className={isBigSize ? 'titleDark' : 'textBodySemiBold'}>
